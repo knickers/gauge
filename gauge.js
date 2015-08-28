36eeb1e1df0abfef05c88ff81333c02deba664ad
Nick Cox <nickcox1008@gmail.com>
2015-08-28 15:47:54 -0600

    Initial options idea

---
+++ gauge.js
@@ -0,0 +1,41 @@
+var opts = {
+	bezel: {
+		style: '',
+		color: '',
+	},
+	background: {
+	},
+	outerMeter: {
+		start: 0,
+		end: 60,
+		majorTick: 10,
+		minorTick: 5,
+		unitTick: 1,
+		label: 'MPH'
+		color: '#ffffff'
+	},
+	ring: {
+		color: '#ffffff',
+		width: '2px'
+	}
+	innerMeter: {
+		start: 0,
+		end: 100,
+		majorTick: 10,
+		minorTick: 5,
+		unitTick: 1,
+		label: 'km/h'
+		color: '#ffffff'
+	},
+	centerMeter: {
+		start: 0,
+		end: 60,
+		majorTick: 10,
+		minorTick: 5,
+		unitTick: 1,
+		label: 'MPH'
+		color: '#ffffff'
+	},
+	indicator: {
+	}
+};
